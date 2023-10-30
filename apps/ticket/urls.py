0b97389eccb07a02ed7823ddf3177b67b9870eb0
amirshariati <seyed.amir.shariati@gmail.com>
2023-10-30 05:02:14 +0330

    add endpoints for models

--- apps/ticket/urls.py
+++ apps/ticket/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path, include
-
+from .views import (
+    StadiumListView,
+    MatchListView,
+    SeatListView,
+    TicketPurchaseListView
+)
 
 urlpatterns = [
+    path("stadium/", StadiumListView.as_view(), name="stadium-list"),
+    path("match/", MatchListView.as_view(), name="match-list"),
+    path("seat/", SeatListView.as_view(), name="seat-list"),
+    path("ticket/", TicketPurchaseListView.as_view(), name="ticket-list"),
 ]
