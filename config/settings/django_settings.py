8d6245c7987a2531a3d27d9d790c3a8c4c5a932f
amirshariati <seyed.amir.shariati@gmail.com>
2023-10-30 05:02:35 +0330

    add redis cache setting

--- config/settings/django_settings.py
+++ config/settings/django_settings.py
@@ -176,3 +176,13 @@ AUTH_USER_MODEL = "accounts.UserAccount"
 
 SITE_ID = 1
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
